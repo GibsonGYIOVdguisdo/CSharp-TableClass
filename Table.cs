a26c45f05640d87b96035483c3cb87ca241c46ed
Gibson <114831858+GibsonGYIOVdguisdo@users.noreply.github.com>
2024-01-03 12:14:14 +0000

    Updated

--- Table.cs
+++ Table.cs
@@ -1,6 +1,30 @@
 class Table{
     private IDictionary<string, List<string>> data = new Dictionary<string, List<string>>();
 
+
+
+    public string[] splitCsvLine(string line){
+        bool dontSplit = false;
+        string currentString = "";
+        List<string> splitString = new List<string>();
+        foreach(char l in line){
+            if (l == '"'){
+                dontSplit = true;
+            }
+            if (l == ',' && dontSplit == false){
+                splitString.Add(currentString);
+                currentString = "";
+            }
+            else{
+                currentString += l;
+            }
+        }
+        if (currentString.Length>0){
+            splitString.Add(currentString);
+        }
+        return splitString.ToArray();
+    
+    }
     public void fromCSV(string filePath){
         IDictionary<string, List<string>> tempData = new Dictionary<string, List<string>>();
         string[] fileContent = File.ReadAllText(filePath).Split("\n");
@@ -13,7 +37,8 @@ class Table{
             string[] recordData = fileContent[i].Split(",");
 
             for (int j = 0; j < recordData.Length; j++){
-                tempData[tempData.Keys.ToArray()[j]].Add(recordData[j]); 
+                Console.WriteLine(recordData[j]);
+                // tempData[tempData.Keys.ToArray()[j]].Add(recordData[j]); 
             }
         }
         this.data = tempData;
